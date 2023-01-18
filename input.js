4bc14c447c23a046b848e0471930505ef4faa48d
Leonardo Picchi Dancona <lpdancona@gmail.com>
2023-01-18 15:41:02 -0800

    WASD Movement

--- input.js
+++ input.js
@@ -1,3 +1,5 @@
+const connect = require("./client");
+const conn = connect();
 const Input = {
   setupInput: function () {
     const stdin = process.stdin;
@@ -11,6 +13,26 @@ const Input = {
     console.log(data);
     if (data === "\u0003") {
       process.exit();
+    }
+    if (data === "w") {
+      // connect.on("data", () => {
+      conn.write("Move: up");
+      // });
+    }
+    if (data === "a") {
+      // connect.on("data", () => {
+      conn.write("Move: left");
+      // });
+    }
+    if (data === "s") {
+      // connect.on("data", () => {
+      conn.write("Move: down");
+      // });
+    }
+    if (data === "d") {
+      // connect.on("data", () => {
+      conn.write("Move: rigth");
+      // });
     }
   },
 };
